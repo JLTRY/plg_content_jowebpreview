6a704bc00711c2932913c60a1f5074e7daab10c6
JLTRY <jltryoen@orange.fr>
2026-02-10 21:00:08 +0100

    fixvd.sh + date

--- fixvd.sh
+++ fixvd.sh
@@ -15,7 +15,7 @@ DATE="$(date "+%B %e, %Y")"
 echo "fixvd $1 with version :$VERSION date:$DATE";
 
 sed -i 's%\(<version>\)[^<]*\(<\/version>\)%\1'$VERSION'\2%' $FILE
-sed -i 's%^\(<creationDate>\)[^<]*\(</creationDate>\)%\1'"$DATE"'\2%' $FILE
+sed -i 's%\(<creationDate>\)[^<]*\(<\/creationDate>\)%\1'"$DATE"'\2%' $FILE
 sed -i 's%^\*\*Version .+ - [^\*]+[0-9]+\*\*$%**Version '$VERSION' - '"$DATE"'**%' $FILE
 sed -i 's%\([v\-]*\)[0-9]\.[0-9]\.[0-9]%\1'$VERSION'%g' $FILE
 
